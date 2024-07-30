a3a4cb83f5f90421530e826681977050df114c05
AbtahiHasan <anikidpersonal@gmail.com>
2024-07-30 12:42:59 +0600

    router setup done

--- main.go
+++ main.go
@@ -25,12 +25,20 @@ func main() {
 	router := chi.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{"https://*","http://*"},
-		AllowedMethods: []string{"GET","POST","PUT","DELETE","OPTIONS"},
-		AllowedHeaders: []string{"*"},
-		AllowCredentials: false,
-		MaxAge: 300,
+		AllowedOrigins: 	[]string{"https://*","http://*"},
+		AllowedMethods: 	[]string{"GET","POST","PUT","DELETE","OPTIONS"},
+		AllowedHeaders: 	[]string{"*"},
+		ExposedHeaders: 	[]string{"Link"},
+		AllowCredentials: 	false,
+		MaxAge: 			300,
 	}))
+
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/health", handlerReadiness)
+
+	router.Mount("/v1",v1Router)
 
 	server := &http.Server{
 		Handler: router,
