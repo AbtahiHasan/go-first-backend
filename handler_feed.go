3c45e5e0baad98c46b9e64eb9cc9010d3636da9a
AbtahiHasan <anikidpersonal@gmail.com>
2024-07-30 21:35:18 +0600

    create feed done

---
+++ handler_feed.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/AbtahiHasan/go-first-backend/internal/database"
+	"github.com/google/uuid"
+)
+
+func (apiCfg *apiConfig) handleCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
+	type parameters struct {
+		Name 	string 	`json:"name"`	
+		Url		string 	`json:"url"`
+	}
+	params := parameters{}
+	err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		errorFormatter(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
+		return
+	}
+
+	newFeed, feedCreatingErr := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+		ID: uuid.New(),
+		Name: params.Name,
+		Url: params.Url,
+		UserID: user.ID,
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+	})
+
+	if feedCreatingErr != nil {
+		errorFormatter(w, 400, fmt.Sprintf("Error creating feed: %v", feedCreatingErr))
+		return
+	}
+
+	responseFormatter(w, 201, newFeed)
+}
