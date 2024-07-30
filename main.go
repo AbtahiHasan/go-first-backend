c13eb145e381d25c09f64cc6f12df9fa685c1a00
AbtahiHasan <anikidpersonal@gmail.com>
2024-07-30 12:11:43 +0600

    server done

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
+
 func main() {
 
+	godotenv.Load()
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("Port not found")
+	}
+
+
+	router := chi.NewRouter()
+
+	server := &http.Server{
+		Handler: router,
+		Addr: ":"+port,
+	}
+	
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Server running on port", port)
 }
